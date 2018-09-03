32fb3a18b769120c1abad73113e7fdd862b0edac
Chris Park-kennaby <chrispkennaby@gmail.com>
2018-09-03 21:17:32 +0100

    restrain dynamic protocol logic to appengine (.appspot.com)

--- main.py
+++ main.py
@@ -252,7 +252,7 @@ class RefreshHandler(webapp2.RequestHandler):
   def get(self):
     # get host for callback URL - if testing locally set server_url to your YT Upload Claimer host (e.g. https://example.com)
     server_url = app_identity.get_default_version_hostname()
-    protocol = 'https://' if server_url.count('.') == 1 else 'http://'
+    protocol = 'https://' if server_url.endswith('.appspot.com') and server_url.count('.') == 1 else 'http://'
     count = 0
 
     for channel_id in CHANNEL_IDS:
